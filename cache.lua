07edbf4fc7351cbf6daae69f0e09b08c3b7f3d6c
R3R3R3 <24302636+R3R3R3@users.noreply.github.com>
2019-09-16 23:09:22 +0200

    Cache force-flushing + cache coherency bugfix
    Cache gets forcibly flushed on shutdown.
    
    Also fixed a tricky bug where a broken reinforcement + immediate place on the
    same block would reload an old DB reinforcement value. Now the cache gets force
    flushed when a reinforced block is broken.

--- cache.lua
+++ cache.lua
@@ -23,38 +23,54 @@ cache:
 local chunk_reinf_cache = {}
 local cache_chunk_expiry = 10 -- debug, can be set to 60+
 
+local function flush_reinf(reinf)
+   local reinf_value = reinf.value
+   local x, y, z = reinf.x, reinf.y, reinf.z
+   if reinf.new then
+      -- minetest.chat_send_all("  Registered: (" .. ptos(x, y, z) .. ")")
+      ctdb.remove_reinforcement(vector.new(x, y, z))
+      -- TODO: cleanup this remove-then-register hack
+      ctdb.register_reinforcement(
+         vector.new(x, y, z),
+         reinf.ctgroup_id,
+         reinf.material
+      )
+      reinf.new = false
+   end
+   if reinf_value < 1 then
+      -- minetest.chat_send_all("  Removed: (" .. ptos(x, y, z) .. ")")
+      ctdb.remove_reinforcement(vector.new(x, y, z))
+   else
+      -- minetest.chat_send_all("  Updated: (" .. ptos(x, y, z) .. ")")
+      ctdb.update_reinforcement(
+         vector.new(x, y, z),
+         reinf_value
+      )
+   end
+end
+
+
 function ct.try_flush_cache()
    local current_time = os.time(os.date("!*t"))
    for key, chunk in pairs(chunk_reinf_cache) do
       if (chunk.time_added + cache_chunk_expiry) < current_time then
          -- minetest.chat_send_all("Flushing chunk (" .. key .. ") to db:")
          for _, reinf in pairs(chunk.reinforcements) do
-            local reinf_value = reinf.value
-            local x, y, z = reinf.x, reinf.y, reinf.z
-            if reinf.new then
-               -- minetest.chat_send_all("  Registered: (" .. ptos(x, y, z) .. ")")
-               ctdb.remove_reinforcement(vector.new(x, y, z))
-               -- TODO: cleanup this remove-then-register hack
-               ctdb.register_reinforcement(
-                  vector.new(x, y, z),
-                  reinf.ctgroup_id,
-                  reinf.material
-               )
-               reinf.new = false
-            end
-            if reinf_value < 1 then
-               -- minetest.chat_send_all("  Removed: (" .. ptos(x, y, z) .. ")")
-               ctdb.remove_reinforcement(vector.new(x, y, z))
-            else
-               -- minetest.chat_send_all("  Updated: (" .. ptos(x, y, z) .. ")")
-               ctdb.update_reinforcement(
-                  vector.new(x, y, z),
-                  reinf_value
-               )
-            end
+            flush_reinf(reinf)
          end
          chunk_reinf_cache[key] = nil
       end
+   end
+end
+
+
+function ct.force_flush_cache()
+   for key, chunk in pairs(chunk_reinf_cache) do
+      -- minetest.chat_send_all("Flushing chunk (" .. key .. ") to db:")
+      for _, reinf in pairs(chunk.reinforcements) do
+         flush_reinf(reinf)
+      end
+      chunk_reinf_cache[key] = nil
    end
 end
 
@@ -89,6 +105,14 @@ function ct.modify_reinforcement(pos, value)
    local vchunk_start = get_pos_chunk(pos)
    local chunk_reinf = chunk_reinf_cache[vtos(vchunk_start)]
    if value < 1 then
+      -- We have to force a flush to get the DB in sync with the removal of the
+      -- node's entry in the cache.
+      --
+      -- If this isn't done, Citadella would reload the value from the DB, which
+      -- is almost certainly not coherent with the current state of the cache.
+      chunk_reinf.reinforcements[vtos(pos)].value = 0
+      ct.force_flush_cache()
+      -- Once the cache has been flushed, this reinforcement entry is removed.
       chunk_reinf.reinforcements[vtos(pos)] = nil
    else
       chunk_reinf.reinforcements[vtos(pos)].value = value
@@ -110,3 +134,8 @@ function ct.register_reinforcement(pos, ctgroup_id, item_name, resource_limit)
       }
    end
 end
+
+
+minetest.register_on_shutdown(function()
+      ct.force_flush_cache()
+end)
